3ce9d289183c6c808a90fed1b4958a495d3579f6
sal-uva <s.h.hagen@uva.nl>
2026-03-24 19:11:03 +0100

    fix: build separator cross-compatability

--- build.py
+++ build.py
@@ -1,14 +1,16 @@
 import subprocess
 import os
 
+separator = os.pathsep
+
 subprocess.run([
     "pyinstaller",
     "main.py",
     "--name", "Kenniskrabber",
     "--onedir",
-    "--windowed",
-    "--add-data", "assets;assets",
-    "--add-data", "README.md;.",
+    "--windowed", # Keep this uncommented for the Mac .app bundle!
+    "--add-data", f"assets{separator}assets",
+    "--add-data", f"README.md{separator}.",
     "--hidden-import", "nicegui",
     "--collect-all", "nicegui",
     "--collect-all", "markdownify",
